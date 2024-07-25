59f312ebbf07adff29329f96a38509a620452753
HibaDoi <doihiba@iav.ac.ma>
2024-07-25 12:18:17 +0200

    localization

---
+++ Localisation/2_Drop_Silimar_point_Localisation.py
@@ -0,0 +1,6 @@
+from utils_localisation import *
+
+
+input_CSV='Localisation\csv_file\_1_raw_points_from_localisation.csv'
+output_CSV='Localisation\csv_file\_2_unique_points_without_duplicat_from_localisation.csv'
+Total_cluster(input_CSV,output_CSV)
